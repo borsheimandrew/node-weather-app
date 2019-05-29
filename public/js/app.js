8c164b7d3e31b9fde2eeb9909107876958dce173
Andrew Borsheim <borsheimandrew@gmail.com>
2019-05-29 11:30:25 -0500

    fixed client fetch call to run on Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,8 @@
-
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const icon = document.querySelector('#icon')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
 
 
 weatherForm.addEventListener('submit', (e) => {
@@ -16,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
 
   messageOne.textContent = 'Loading...'
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+  fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
